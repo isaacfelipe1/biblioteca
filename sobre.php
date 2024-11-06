80baba7db967bd7fd92e6ca8bcf285056046d9d2
isaacfelipe1 <ifdsl.lic20@uea.edu.br>
2024-11-06 16:14:24 -0300

    feat: reajustando os links e o destaque

--- sobre.php
+++ sobre.php
@@ -5,5 +5,5 @@
     <p>Um espaço da produção de diferentes manifestações artísticas. Além de propiciar o acesso a diversidade literária
         para a comunidade, a biblioteca realiza rodas de leituras, rodas musicais, concurso de poesia e desenho e
         oficinas de contação de histórias.</p>
-    <button class="btn-sobre">Saiba Mais</button>
+    <a href="how_it_started.php" class="btn-sobre"><button class="btn-sobre">Saiba Mais</button></a>
 </section>
