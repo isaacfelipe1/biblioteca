d75738bc18c4bb055863d1f85a2eea4db2966339
isaacfelipe1 <ifdsl.lic20@uea.edu.br>
2024-11-06 16:23:06 -0300

    feat: adicionada fotos para o template

--- how_it_started.php
+++ how_it_started.php
@@ -23,20 +23,21 @@
     <main class="main-content">
         <section class="about-maria-dolores">
             <div class="about-content">
-                <img src="assets/maria-dolores.jpg" alt="Maria Dolores" class="about-image">
+                <img src="assets/maria_dolores.jpg" alt="Maria Dolores" class="about-image">
                 <div class="text-content">
                     <h2>Quem foi Maria Dolores?</h2>
                     <p>Mentora espiritual do Centro Espírita Maria Dolores, escritora e poetisa que lutou pela causa das
                         mulheres e meninas órfãs.</p>
                     <p>Saiba mais, acesse o link abaixo:</p>
-                    <a href="#" class="learn-more-link">Conheça a história do Centro Espírita Maria Dolores</a>
+                    <a href="https://feamazonas.org.br/noticias-das-casas/conheca-historia-do-centro-espirita-maria-dolores/"
+                        class="learn-more-link" target="_blank">Conheça a história do Centro Espírita Maria Dolores</a>
                 </div>
             </div>
         </section>
         <section class="logo-meaning">
             <h2>Conheça o significado dos elementos da logomarca da biblioteca.</h2>
             <div class="logo-image-container">
-                <img src="assets/logo-meaning.jpg" alt="Significado da Logomarca da Biblioteca">
+                <img src="assets/logo01.jpg" alt="Significado da Logomarca da Biblioteca">
             </div>
         </section>
         <section class="book-loan">
@@ -44,10 +45,9 @@
             <p>Venha nos visitar e aproveite para conhecer nosso acervo literário.</p>
             <p>Empréstimo de livros – segunda à sábado.<br>Horário: 08h às 12h | 14h às 17h</p>
             <div class="book-images-container">
-                <img src="assets/books1.jpg" alt="Acervo de livros 1">
-                <img src="assets/books2.jpg" alt="Acervo de livros 2">
-                <img src="assets/books3.jpg" alt="Acervo de livros 3">
-                <img src="assets/books4.jpg" alt="Acervo de livros 4">
+                <img src="assets/livros01.jpg" alt="Acervo de livros 1">
+                <img src="assets/livros02.jpg" alt="Acervo de livros 2">
+
             </div>
             <div class="loan-banner">
                 <h3>Empréstimo de livro?</h3>
